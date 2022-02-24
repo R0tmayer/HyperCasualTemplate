a5f4142dd877bed837b1bc3cba8cfef84cbf1bff
Roman Salnikov <r.salnikov1998@gmail.com>
2022-02-24 17:16:31 +1000

    Code refactoring

--- Assets/Scripts/Core/Upgrades/StatPresenter.cs
+++ Assets/Scripts/Core/Upgrades/StatPresenter.cs
@@ -15,8 +15,8 @@
         public int SpeedValue => _upgradesUpgradesConfig.SpeedList[_statLevelSaver.SpeedLevel].Value;
         public int ArmorValue => _upgradesUpgradesConfig.ArmorList[_statLevelSaver.ArmorLevel].Value;        
         
-        public int PowerCost => _upgradesUpgradesConfig.PowerList[_statLevelSaver.PowerLevel].Value;
-        public int SpeedCost => _upgradesUpgradesConfig.SpeedList[_statLevelSaver.SpeedLevel].Value;
-        public int ArmorCost => _upgradesUpgradesConfig.ArmorList[_statLevelSaver.ArmorLevel].Value;
+        public int PowerCost => _upgradesUpgradesConfig.PowerList[_statLevelSaver.PowerLevel].Cost;
+        public int SpeedCost => _upgradesUpgradesConfig.SpeedList[_statLevelSaver.SpeedLevel].Cost;
+        public int ArmorCost => _upgradesUpgradesConfig.ArmorList[_statLevelSaver.ArmorLevel].Cost;
     }
 }
