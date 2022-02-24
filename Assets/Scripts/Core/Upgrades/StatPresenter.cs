40729cc89967634fd27b00d9239e0500a8af978b
Roman Salnikov <r.salnikov1998@gmail.com>
2022-02-24 17:18:36 +1000

    Code refactoring

--- Assets/Scripts/Core/Upgrades/StatPresenter.cs
+++ Assets/Scripts/Core/Upgrades/StatPresenter.cs
@@ -2,21 +2,21 @@
 {
     public class StatPresenter
     {
-        private readonly UpgradesConfig _upgradesUpgradesConfig;
+        private readonly UpgradesConfig _upgradesConfig;
         private readonly StatLevelSaver _statLevelSaver;
 
-        public StatPresenter(UpgradesConfig upgradesUpgradesConfig, StatLevelSaver statLevelSaver)
+        public StatPresenter(UpgradesConfig upgradesConfig, StatLevelSaver statLevelSaver)
         {
             _statLevelSaver = statLevelSaver;
-            _upgradesUpgradesConfig = upgradesUpgradesConfig;
+            _upgradesConfig = upgradesConfig;
         }
 
-        public int PowerValue => _upgradesUpgradesConfig.PowerStatList[_statLevelSaver.PowerLevel].Value;
-        public int SpeedValue => _upgradesUpgradesConfig.SpeedStatList[_statLevelSaver.SpeedLevel].Value;
-        public int ArmorValue => _upgradesUpgradesConfig.ArmorStatList[_statLevelSaver.ArmorLevel].Value;        
+        public int PowerValue => _upgradesConfig.PowerStatList[_statLevelSaver.PowerLevel].Value;
+        public int SpeedValue => _upgradesConfig.SpeedStatList[_statLevelSaver.SpeedLevel].Value;
+        public int ArmorValue => _upgradesConfig.ArmorStatList[_statLevelSaver.ArmorLevel].Value;        
         
-        public int PowerCost => _upgradesUpgradesConfig.PowerStatList[_statLevelSaver.PowerLevel].Cost;
-        public int SpeedCost => _upgradesUpgradesConfig.SpeedStatList[_statLevelSaver.SpeedLevel].Cost;
-        public int ArmorCost => _upgradesUpgradesConfig.ArmorStatList[_statLevelSaver.ArmorLevel].Cost;
+        public int PowerCost => _upgradesConfig.PowerStatList[_statLevelSaver.PowerLevel].Cost;
+        public int SpeedCost => _upgradesConfig.SpeedStatList[_statLevelSaver.SpeedLevel].Cost;
+        public int ArmorCost => _upgradesConfig.ArmorStatList[_statLevelSaver.ArmorLevel].Cost;
     }
 }
